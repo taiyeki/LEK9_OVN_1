c5cfd4e8934a69295ee7c3cbd0f554c040c42beb
Mustafa Maher <mustafa.maher@elev.ga.ntig.se>
2023-11-27 08:58:56 +0100

    Gjorde 9_1_a On branch main

---
+++ src/Lek9_1_A.java
@@ -0,0 +1,15 @@
+public class Lek9_1_A {
+    public static void main(String[] args) throws Exception {
+
+        double omkrets = omkrets1(9);
+        System.out.println("Omkretsen av cirkeln är "+ omkrets);
+    }
+        static double omkrets1(double diameter)
+        {
+            double asdf = diameter*Math.PI;
+            return asdf;
+        }
+        
+
+    }
+
---
+++ src/Lek9_1_B.java
@@ -0,0 +1,21 @@
+public class Lek9_1_B {
+    
+}
+public static void main(String[] args) throws Exception {
+
+
+
+
+
+
+
+}
+
+{
+    static void ritalinje(int längd)
+    {
+        
+    }
+
+
+}
